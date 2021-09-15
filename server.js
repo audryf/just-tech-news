bcf1f10ac868664d1c2e2300b555c1edf6e430e2
audryf <84256033+audryf@users.noreply.github.com>
2021-09-15 15:00:46 -0500

    add helpers and tests

--- server.js
+++ server.js
@@ -21,13 +21,15 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers'));
